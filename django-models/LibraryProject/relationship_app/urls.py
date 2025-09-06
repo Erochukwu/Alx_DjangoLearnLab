577c4cdbd6c10b2665d0e4b52b942108d6a8ef6b
Erochukwu <fortifiedmercy@gmail.com>
2025-09-06 23:18:08 +0100

    Add user authentication update file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import list_books_function_view, LibraryDetailView, register, login
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view → lists all books
-    path("books-fbv/", list_books_function_view, name="books_fbv"),
+    path("books-fbv/", views.list_books_function_view, name="books_fbv"),
 
     # Class-based view → details of a specific library
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     
-    # ...other patterns...
-    path("login/", login, name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
+    # Login and logout views
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    
+    # Registration view
+    path("register/", views.register, name="register"),
 ]
