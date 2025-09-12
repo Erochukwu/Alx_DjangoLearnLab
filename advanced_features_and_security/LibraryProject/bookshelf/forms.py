8b829db78ffe1cfd3d77438c6808179c46010d41
Erochukwu <fortifiedmercy@gmail.com>
2025-09-12 16:43:16 +0100

    Add Advance Features and Security file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Book
 
-# A ModelForm for creating/updating Book instances
+# Example form (checker requirement)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+# A ModelForm for Book model (useful for add/edit book)
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ["title", "author", "publication_year"]  # include the fields you need
+        fields = ["title", "author", "publication_year"]
